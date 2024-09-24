370f863eb24f6162341e5cc01ce2d3846ccd5dce
Shriradhey <129725024+Shriradhey@users.noreply.github.com>
2024-09-24 15:39:06 +0530

    Update script.js

--- admin/testDonations/script.js
+++ admin/testDonations/script.js
@@ -1,20 +1,24 @@
-const form = document.querySelector("#donationsLoginForm")
+const form = document.getElementById("donationsLoginForm")
+// const btn = document.querySelector('#show')
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
     let username = document.getElementById('key').value;
     let password = document.getElementById('secret').value;
 
     let xhr = new XMLHttpRequest()
-    xhr.open('GET', 'https://api.razorpay.com/v1/payments')
-    let auth = window.btoa(username +':'+password)
-    xhr.setRequestHeader('Authorization','Basic '+auth)
-    xhr.send()
+    xhr.open('GET', 'https://api.razorpay.com/v1/payments/', true, username, password)
+
+    // let auth = window.btoa(username +':'+password)
+    // xhr.setRequestHeader('Authorization','Basic '+auth)
+
 
     xhr.onload = () =>{
         if(xhr.status ==200){
             document.getElementById('data').innerHTML = xhr.response
         }
     }
+
+    xhr.send()
     console.log(xhr)
 
 
